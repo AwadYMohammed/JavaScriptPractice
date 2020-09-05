2c3ea2bde887a827e5d216dcac40cacd2fcdeb8e
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-05 13:23:18 -0400

    use console.log() to print to the console

--- JS.js
+++ JS.js
@@ -1,2 +1,3 @@
 et newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 const  hobbies  = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+console.log(hobbies)
