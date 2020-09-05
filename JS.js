0d3a488af19f8d7cce50f51663eb6996df0a1ac3
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-05 13:25:55 -0400

    git

--- JS.js
+++ JS.js
@@ -1,3 +1,8 @@
 et newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 const  hobbies  = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 console.log(hobbies)
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem  = famousSayings[0];
+console.log(listItem);
