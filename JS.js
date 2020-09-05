707e49b0359aab99f989b6b8557e714d095a766c
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-05 14:09:53 -0400

    Accessing nested array elements

--- JS.js
+++ JS.js
@@ -174,3 +174,7 @@ console.log(nestedArr[1][0]); // Output: 2
 In the second console.log() statement, we have two bracket notations chained to nestedArr. We know that nestedArr[1] is the array [2, 3]. Then to grab the first element from that array, we use nestedArr[1][0] and we get the value of 2.
 
 */
+
+onst numberClusters = [[1,2], [3, 4],[5, 6]];
+const target  = numberClusters[2][1];
+console.log(target);
