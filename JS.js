51f36778a52b72fe003d65fc446a779747fdd477
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-05 13:34:55 -0400

    const vs let array variable

--- JS.js
+++ JS.js
@@ -6,3 +6,17 @@ const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious th
 
 let listItem  = famousSayings[0];
 console.log(listItem);
+console.log(famousSayings[3]);
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+condiments = ['Mayo'];
+utensils[3] = 'Spoon';
+console.log(utensils[3])
