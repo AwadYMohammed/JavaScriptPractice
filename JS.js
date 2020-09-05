8f1fecb6ca4778e2605d3aaa9ff52db0b878845a
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-05 13:38:01 -0400

    the .push method

--- JS.js
+++ JS.js
@@ -20,6 +20,15 @@ console.log(condiments);
 condiments = ['Mayo'];
 utensils[3] = 'Spoon';
 console.log(utensils[3])
+//The .push() Method
+/*Let’s learn about some built-in JavaScript methods that make working with arrays easier. These methods are specifically called on arrays to make common tasks, like adding and removing elements, more straightforward.
 
+One method, .push() allows us to add items to the end of an array. Here is an example of how this is used:*/
 const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
 console.log(objectives.length);
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+
+itemTracker.push('item 3', 'item 4');
+
+console.log(itemTracker);
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
