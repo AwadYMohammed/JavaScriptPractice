c0ea97d1ea5b28cf18a3180ff92901719960ce84
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-08 15:46:47 -0400

    map function

--- JS.js
+++ JS.js
@@ -253,3 +253,22 @@ const onlyNumbers = things.filter(thing => {
 });
 
 console.log(onlyNumbers);
+
+
+/////////////////
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => {
+  return animal[0];
+});
+
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(bigNumber => {
+  return bigNumber/100;
+});
